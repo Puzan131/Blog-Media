a1f08559ec35817ef0aa1b56e6ffdad57f46362f
kabya bhatt <kabyabhatt4@gmail.com>
2025-02-10 21:47:20 +0545

    redux setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,35 @@
-import { useState } from 'react'
-
-
+import { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { login, logout } from "./features/authSlice";
+import authService from "./appwrite/auth";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 function App() {
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login(userData));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => setLoading(false));
+  }, []);
 
-
-  return (
-    <>
-    <div>hey</div>
-    </>
-  )
+  //conditional rendering
+  return loading ? null : (
+  <div className="flex  flex-wrap min-h-screen bg-fuchsia-300">
+    <div className="w-full block ">
+        <Header />
+         {/*  <Outlet /> */}
+        <Footer />
+    </div>
+  </div>
+);
 }
 
-export default App
+export default App;
